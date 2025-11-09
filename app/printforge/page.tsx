cebc8ad09696159eeec9873556dd1ecc3428ff88
matrix-node <rytchiemacharia@gmail.com>
2025-11-09 20:46:54 +0300

    first commit

---
+++ app/printforge/page.tsx
@@ -0,0 +1,18 @@
+
+
+export default function PrintForgePage() {
+  return (
+    <>
+    <header className="flex justify-between items-center p-4 bg-gray-800">
+        <img src="" alt="" />
+        <nav>
+            <li>3D Models</li>
+            <li>About</li>
+        </nav>
+    </header>
+    <main>
+      <h1>Welcome to the PrintForge Page</h1>
+    </main>
+    </>
+  )
+}
